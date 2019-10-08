abd10dee6310d9dc10517fc47c535e222239ef67
Amani Berkani <amaniberkani@icloud.com>
2019-10-08 16:31:54 +0200

    DOJO

--- src/kata/Arena.java
+++ src/kata/Arena.java
@@ -1,8 +1,14 @@
 public class Arena {
+	
 
-    public static String fight(Worm worm1, Worm worm2) {
+	public static void main(String[] args){
+		Worm worm1 = new Worm("worm1", 100, 10);
+		Worm worm2 = new Worm("worm2", 100, 20);
 
-        // TODO
-        return null;
-    }
+		while(worm1.getLife() > 0 && worm2.getLife() > 0){
+			worm1.fight(worm2);
+		}
+
+	}
+
 }
--- src/kata/Worm.java
+++ src/kata/Worm.java
@@ -1,8 +1,65 @@
 public class Worm {
 
-    // TODO
+	private String name;
+	private int life;
+	private int attack;
 
-    public Worm(String name, int life, int attack) {
-        // TODO
-    }
+	public Worm(String name, int life, int attack) {
+		
+		this.name = name;
+		this.life = life;
+		this.attack = attack;
+	
+    	}
+	// Getter
+	public String getName() {
+		return this.name;
+	}
+
+	public int getLife() {
+		return this.life;
+	}
+
+	public int getAttack() {
+		return this.attack;
+	}
+	
+	// Setter
+	public void setName(String name) {
+		this.name=name;
+	}
+	public void setLife( int life) {
+		this.life=life;
+	}
+	public void setAttack(int attack) {
+		this.attack=attack;
+	}
+
+	public void fight(Worm adverse) {
+		if(this.life <= adverse.life && this.life>0 && adverse.life>0){
+			System.out.println("Worm1 attacks first !");
+			this.life-=adverse.attack;
+			adverse.life=adverse.life - this.attack;
+						
+				
+		}else if (this.life > adverse.life && this.life>0 && adverse.life>0 ){	
+			System.out.println("Worm2 attacks first !");
+			adverse.life=adverse.life - this.attack;
+			this.life-=adverse.attack;
+						
+		}
+
+		if (this.life<=0) {
+			System.out.println(this.name+ " has " + this.life+" points remaining ");
+			System.out.println(adverse.name+  " has " + adverse.life+" points remaining ");
+			System.out.println(adverse.name + " is the winner !!!! ");
+		} else {
+			System.out.println(this.name+ " : Points remaining " + this.life);
+			System.out.println(adverse.name+ " : Points remaining " + adverse.life);
+		}
+
+		
+
+	}	
+		
 }
