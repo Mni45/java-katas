9cf774df3a0e728be7e313e2d64a9aae6d37c9b8
Amani Berkani <amaniberkani@icloud.com>
2020-01-23 10:38:36 +0100

    done

--- src/kata/Factorial.java
+++ src/kata/Factorial.java
@@ -1,4 +1,36 @@
 public class Factorial {
 
-    // TODO
+public static int factoriel (int n) {		
+		
+		if (n<0 ) 
+			throw new IllegalArgumentException("n must be positive");
+		
+		
+		int resultat=1;
+		
+		for (int i=1 ; i<=n ; i++) {
+			resultat*=1*i;
+
+		}
+		
+		return resultat;		
+	
+	}
+
+	public static int recursiveFactoriel ( int n) {
+		
+		if (n<0 ) 
+			throw new IllegalArgumentException("n must be positive");
+		
+		int factoriel;
+		if (n == 0)
+			factoriel = 1;
+		 
+		else
+			
+			factoriel = n*recursiveFactoriel(n-1);
+		
+		return factoriel;
+		
+	}
 }
--- src/test/FactorialTest.java
+++ src/test/FactorialTest.java
@@ -3,10 +3,29 @@ import org.junit.Test;
 
 public class FactorialTest {
 
-    @Test
-    public void testTrue() throws Exception {
-        Assert.assertEquals(true, true);
-    }
 
-    // TODO
+    @Test(expected = IllegalArgumentException.class)
+    public void testIsNegative() throws Exception {
+        Factorial.factoriel(-5);
+        Factorial.recursiveFactoriel(-5);
+
+    }
+    
+    @Test
+    public void testNominalCase() throws Exception {
+	  Assert.assertEquals(1,Factorial.factoriel(0));
+	  Assert.assertEquals(1,Factorial.factoriel(1));
+	  Assert.assertEquals(2,Factorial.factoriel(2));
+	  Assert.assertEquals(6,Factorial.factoriel(3));
+	  Assert.assertEquals(24,Factorial.factoriel(4));
+	  Assert.assertEquals(120,Factorial.factoriel(5));
+	  Assert.assertEquals(1,Factorial.recursiveFactoriel(0));
+	  Assert.assertEquals(1,Factorial.recursiveFactoriel(1));
+	  Assert.assertEquals(2,Factorial.recursiveFactoriel(2));
+	  Assert.assertEquals(6,Factorial.recursiveFactoriel(3));
+	  Assert.assertEquals(24,Factorial.recursiveFactoriel(4));
+	  Assert.assertEquals(120,Factorial.recursiveFactoriel(5));
+    }
+   
+
 }
